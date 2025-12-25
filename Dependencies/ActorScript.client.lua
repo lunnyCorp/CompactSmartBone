bca040b70d7a1df4fa4eebd7d825812155c5652b
Lunny Nymphet <99461156+LARTAJE@users.noreply.github.com>
2025-12-25 05:11:52 -0300

    Update ActorScript.client.lua

--- Dependencies/ActorScript.client.lua
+++ Dependencies/ActorScript.client.lua
@@ -1,140 +1,142 @@
-if not script.Parent:IsA("Actor") then
-	return
-end
+task.spawn(function()
+	repeat
+		task.wait()
+	until script.Parent and script.Parent:IsA("Actor")
 
--- // Types \\ --
+	-- // Types \\ --
 
-type dictionary = { [string]: any }
-type array = { [number]: any }
+	type dictionary = { [string]: any }
+	type array = { [number]: any }
 
--- // Objects \\ --
+	-- // Objects \\ --
 
-local Event = script.Parent.Event
+	local Event = script.Parent.Event
 
--- // Services \\ --
+	-- // Services \\ --
 
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+	local Players = game:GetService("Players")
+	local RunService = game:GetService("RunService")
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
--- // Dependencies \\ --
+	-- // Dependencies \\ --
 
-local Paths = {
-	ReplicatedStorage,
-	Players.LocalPlayer:WaitForChild("PlayerScripts"),
-}
-local Require = getgenv().sharedRequire
-local mrp = 'https://raw.githubusercontent.com/LARTAJE/CompactSmartBone/refs/heads/main/'
+	local Paths = {
+		ReplicatedStorage,
+		Players.LocalPlayer:WaitForChild("PlayerScripts"),
+	}
+	local Require = getgenv().sharedRequire
+	local mrp = 'https://raw.githubusercontent.com/LARTAJE/CompactSmartBone/refs/heads/main/'
 
-local Config = Require(mrp..'Dependencies/Config.lua')
+	local Config = Require(mrp..'Dependencies/Config.lua')
 
-local SmartBone = Require(mrp..'init.lua')
-local CameraUtil = Require(mrp..'Dependencies/CameraUtil.lua')
+	local SmartBone = Require(mrp..'init.lua')
+	local CameraUtil = Require(mrp..'Dependencies/CameraUtil.lua')
 
-local DEBUG = Config.Debug
+	local DEBUG = Config.Debug
 
-local timeFunc = os.clock
-local oldTime = timeFunc()
-local frameRate = 60
-local frameRateTable = {}
+	local timeFunc = os.clock
+	local oldTime = timeFunc()
+	local frameRate = 60
+	local frameRateTable = {}
 
---[[ Local Functions ]] --
+	--[[ Local Functions ]] --
 
-local round = 1000
+	local round = 1000
 
-local function roundNumber(num)
-	return  math.floor((num * round) + 0.5) / round
-end
-
-local function smoothDelta()
-	local currentTime = timeFunc()
-
-	for index = #frameRateTable,1,-1 do
-		frameRateTable[index + 1] = (frameRateTable[index] >= currentTime - 1) and frameRateTable[index] or nil
+	local function roundNumber(num)
+		return  math.floor((num * round) + 0.5) / round
 	end
 
-	frameRateTable[1] = currentTime
-	frameRate =  math.floor((timeFunc() - oldTime >= 1 and #frameRateTable) or (#frameRateTable / (timeFunc() - oldTime)))
+	local function smoothDelta()
+		local currentTime = timeFunc()
 
-	return roundNumber(frameRate * ((1/frameRate)^2) + .001)
-end
+		for index = #frameRateTable,1,-1 do
+			frameRateTable[index + 1] = (frameRateTable[index] >= currentTime - 1) and frameRateTable[index] or nil
+		end
 
-local function Initialize(Object: BasePart, RootList: array)
-	local SBone = SmartBone.new(Object, RootList)
+		frameRateTable[1] = currentTime
+		frameRate =  math.floor((timeFunc() - oldTime >= 1 and #frameRateTable) or (#frameRateTable / (timeFunc() - oldTime)))
 
-	local frameTime = 0
+		return roundNumber(frameRate * ((1/frameRate)^2) + .001)
+	end
 
-	SBone.SimulationConnection = RunService.Heartbeat:ConnectParallel(function(Delta: number)
-		Delta = smoothDelta()
-		frameTime += Delta
+	local function Initialize(Object: BasePart, RootList: array)
+		local SBone = SmartBone.new(Object, RootList)
 
-		local camPosition = workspace.CurrentCamera.CFrame.Position
-		local rootPosition = SBone.RootPart.Position
-		local throttleDistance = SBone.Settings.ThrottleDistance
-		local distance = (camPosition - rootPosition).Magnitude
-		local activationDistance = SBone.Settings.ActivationDistance
+		local frameTime = 0
 
-		local updateDistance = math.clamp(distance - throttleDistance, 0, activationDistance)
-		local updateThrottle = 1 - math.clamp(updateDistance / activationDistance, 0, 1)
+		SBone.SimulationConnection = RunService.Heartbeat:ConnectParallel(function(Delta: number)
+			Delta = smoothDelta()
+			frameTime += Delta
 
-		local UpdateRate = math.floor(math.clamp(updateThrottle * SBone.Settings.UpdateRate, 1, SBone.Settings.UpdateRate))
+			local camPosition = workspace.CurrentCamera.CFrame.Position
+			local rootPosition = SBone.RootPart.Position
+			local throttleDistance = SBone.Settings.ThrottleDistance
+			local distance = (camPosition - rootPosition).Magnitude
+			local activationDistance = SBone.Settings.ActivationDistance
 
-		local WithinViewport = CameraUtil.WithinViewport(SBone.RootPart)
-		if frameTime >= (1/UpdateRate) then
-			if distance < activationDistance and WithinViewport then
-				Delta = frameTime
-				frameTime = 0
+			local updateDistance = math.clamp(distance - throttleDistance, 0, activationDistance)
+			local updateThrottle = 1 - math.clamp(updateDistance / activationDistance, 0, 1)
 
-				debug.profilebegin("SoftBone")
+			local UpdateRate = math.floor(math.clamp(updateThrottle * SBone.Settings.UpdateRate, 1, SBone.Settings.UpdateRate))
 
-				if SBone.InRange == false then
-					SBone.InRange = true
-				end
+			local WithinViewport = CameraUtil.WithinViewport(SBone.RootPart)
+			if frameTime >= (1/UpdateRate) then
+				if distance < activationDistance and WithinViewport then
+					Delta = frameTime
+					frameTime = 0
 
-				SBone:UpdateBones(Delta, UpdateRate)
+					debug.profilebegin("SoftBone")
 
-				debug.profileend()
-
-				task.synchronize()
-
-				debug.profilebegin("SoftBoneTransform")
-
-				for _, _ParticleTree in SBone.ParticleTrees do
-					SBone:TransformBones(_ParticleTree, Delta)
-					if DEBUG then
-						SBone:DEBUG(_ParticleTree, Delta)
+					if SBone.InRange == false then
+						SBone.InRange = true
 					end
-				end
 
-				debug.profileend()
+					SBone:UpdateBones(Delta, UpdateRate)
 
-				task.desynchronize()
-
-			else
-				if SBone.InRange == true then
-					SBone.InRange = false
-
-					for _, _ParticleTree in SBone.ParticleTrees do
-						SBone:ResetParticles(_ParticleTree)
-					end
+					debug.profileend()
 
 					task.synchronize()
 
+					debug.profilebegin("SoftBoneTransform")
+
 					for _, _ParticleTree in SBone.ParticleTrees do
-						SBone:ResetTransforms(_ParticleTree, Delta)
+						SBone:TransformBones(_ParticleTree, Delta)
+						if DEBUG then
+							SBone:DEBUG(_ParticleTree, Delta)
+						end
 					end
 
+					debug.profileend()
+
 					task.desynchronize()
+
+				else
+					if SBone.InRange == true then
+						SBone.InRange = false
+
+						for _, _ParticleTree in SBone.ParticleTrees do
+							SBone:ResetParticles(_ParticleTree)
+						end
+
+						task.synchronize()
+
+						for _, _ParticleTree in SBone.ParticleTrees do
+							SBone:ResetTransforms(_ParticleTree, Delta)
+						end
+
+						task.desynchronize()
+					end
 				end
 			end
-		end
-	end)
+		end)
 
-	return SBone
-end
+		return SBone
+	end
 
---[[ Event Handler ]]--
+	--[[ Event Handler ]]--
 
-Event.OnInvoke = function(Object: BasePart, RootList: array)
-	return Initialize(Object, RootList)
-end
+	Event.OnInvoke = function(Object: BasePart, RootList: array)
+		return Initialize(Object, RootList)
+	end
+end)

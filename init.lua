109914fe7c5fb91f3d445c4744cdc40412704396
Lunny Nymph <99461156+Lunny-Nymph@users.noreply.github.com>
2026-02-02 19:25:43 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -72,7 +72,7 @@ local ZERO = Vector3.zero
 
 -- // Dependencies \\ --
 local Require = getgenv().sharedRequire
-local mrp = 'https://raw.githubusercontent.com/LARTAJE/CompactSmartBone/refs/heads/main'
+local mrp = 'https://raw.githubusercontent.com/lunnyCorp/CompactSmartBone/refs/heads/main'
 
 local Config = Require(mrp..'/Dependencies/Config.lua')
 
